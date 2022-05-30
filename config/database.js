a74d7294c7be58c21aea2b383c1ba570642a5798
Pramod Singh <spramodgusain@gmail.com>
2022-05-30 23:50:15 +0530

    Database update

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
+      host: env('DATABASE_HOST', 'db4free.net'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', ''),
+      database: env('DATABASE_NAME', 'blog_site'),
+      user: env('DATABASE_USERNAME', 'aadigusain'),
+      password: env('DATABASE_PASSWORD', '06@sep1997'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
